6b260035b5e90c28fc786432c7910fc0f8984b3a
rawwr <akshayace0302@gmail.com>
2022-07-15 03:11:07 +0530

    tog fav w context api

---
+++ src/context/products-context.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+
+export const ProductsContext = React.createContext({
+  products: [],
+  toggleFav: (id) => {},
+});
+
+export default (props) => {
+  const [productsList, setProductsList] = useState([
+    {
+      id: "p1",
+      title: "Red Scarf",
+      description: "A pretty red scarf.",
+      isFavorite: false,
+    },
+    {
+      id: "p2",
+      title: "Blue T-Shirt",
+      description: "A pretty blue t-shirt.",
+      isFavorite: false,
+    },
+    {
+      id: "p3",
+      title: "Green Trousers",
+      description: "A pair of lightly green trousers.",
+      isFavorite: false,
+    },
+    {
+      id: "p4",
+      title: "Orange Hat",
+      description: "Street style! An orange hat.",
+      isFavorite: false,
+    },
+  ]);
+
+  const toggleFavorite = (productId) => {
+    setProductsList((currentProdList) => {
+      //   console.log(currentProdList);
+      const prodIndex = currentProdList.findIndex((p) => p.id === productId);
+      //   console.log(prodIndex);
+      const newFavStatus = !currentProdList[prodIndex].isFavorite;
+      //   console.log(newFavStatus);
+      const updatedProducts = [...currentProdList];
+      //   console.log(updatedProducts);
+      updatedProducts[prodIndex] = {
+        ...currentProdList[prodIndex],
+        isFavorite: newFavStatus,
+      };
+
+      return updatedProducts;
+    });
+  };
+
+  return (
+    <ProductsContext.Provider
+      value={{ products: productsList, toggleFav: toggleFavorite }}
+    >
+      {props.children}
+    </ProductsContext.Provider>
+  );
+};
